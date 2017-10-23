a440b10e5a30986e2eb3d8f95d1b6151fdb802d2
Antti Haapala <antti.haapala@anttipatterns.com>
2017-10-23 22:12:32 +0300

    actually implement the bunch

--- tet/mailer/__init__.py
+++ tet/mailer/__init__.py
@@ -1,14 +1,26 @@
+from abc import ABCMeta, abstractmethod
+
 from pyramid.config import Configurator
 from pyramid_mailer import mailer_factory_from_settings
 from pyramid_mailer.message import Message
 from tet.services import RequestScopedBaseService
 
 
-class MailerService(RequestScopedBaseService):
+class IMailerService(metaclass=ABCMeta):
+    @abstractmethod
+    def send_immediately(self, message):
+        pass
+
+    @abstractmethod
+    def send(self, message):
+        pass
+
+
+class MailerService(RequestScopedBaseService, IMailerService):
     def __init__(self, **kw):
         super().__init__(**kw)
         mailer = self.request.registry['tet.mailer.factory']
-        self._mailer = mailer.bind(self.request.tm)
+        self._mailer = mailer.bind(transaction_manager=self.request.tm)
 
     def send(self, message: Message):
         return self._mailer.send(message)
@@ -18,7 +30,11 @@ class MailerService(RequestScopedBaseService):
 
 
 def includeme(config: Configurator) -> None:
+    config.include('tet.services')
     settings = config.registry.settings
     prefix = settings.get('tet.mailer.prefix', 'tet.mailer.')
     mailer = mailer_factory_from_settings(settings, prefix=prefix)
     config.registry['tet.mailer.factory'] = mailer
+    config.register_tet_service(MailerService,
+                                scope='request',
+                                interface=IMailerService)
