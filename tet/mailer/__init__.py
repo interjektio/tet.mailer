55563aa7a9ddf029b4a56acd81391fac1dc4016c
Antti Haapala <antti.haapala@anttipatterns.com>
2017-10-24 22:16:07 +0300

    debugmailer

--- tet/mailer/__init__.py
+++ tet/mailer/__init__.py
@@ -1,9 +1,14 @@
+import logging
+import os
 from abc import ABCMeta, abstractmethod
 
 from pyramid.config import Configurator
 from pyramid_mailer import mailer_factory_from_settings
+from pyramid_mailer.mailer import DebugMailer
 from pyramid_mailer.message import Message
 from tet.services import RequestScopedBaseService
+
+_log = logging.getLogger(__name__)
 
 
 class IMailerService(metaclass=ABCMeta):
@@ -14,6 +19,39 @@ class IMailerService(metaclass=ABCMeta):
     @abstractmethod
     def send(self, message):
         pass
+
+
+class DebugCoalescingMailer(object):
+    def __init__(self, mailer, mailer_debug_path, **bind_kw):
+        self._mailer_debug_path = os.path.abspath(mailer_debug_path)
+
+        args_without_tm = dict(bind_kw)
+        self._tm = args_without_tm.pop('transaction_manager', None)
+        self._main_mailer = mailer.bind(**args_without_tm)
+        self._debug_mailer = DebugMailer(mailer_debug_path).bind(**bind_kw)
+
+    def bind(self, **kw):
+        return self.__class__(
+            mailer=self._main_mailer,
+            mailer_debug_path=self._mailer_debug_path,
+            **kw
+        )
+
+    def send_immediately(self, message: Message) -> None:
+        try:
+            self._main_mailer.send_immediately(message)
+        except Exception as e:
+            _log.exception(e)
+
+        self._debug_mailer.send_immediately(message)
+
+    def send(self, message: Message) -> None:
+        try:
+            self._main_mailer.send_immediately(message)
+        except Exception as e:
+            _log.exception(e)
+
+        self._debug_mailer.send(message)
 
 
 class MailerService(RequestScopedBaseService, IMailerService):
@@ -34,6 +72,11 @@ def includeme(config: Configurator) -> None:
     settings = config.registry.settings
     prefix = settings.get('tet.mailer.prefix', 'tet.mailer.')
     mailer = mailer_factory_from_settings(settings, prefix=prefix)
+
+    debug_path = settings.get(prefix + 'debug_directory')
+    if debug_path:
+        mailer = DebugCoalescingMailer(mailer, debug_path)
+
     config.registry['tet.mailer.factory'] = mailer
     config.register_tet_service(MailerService,
                                 scope='request',
